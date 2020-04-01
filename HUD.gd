138d6e9fd2ee575b468bd75b0be00446f0dd6c58
Abhishek <AbhiSaphire@github.com>
2020-04-01 18:36:03 +0530

    Added enemies and canvas with menu

---
+++ HUD.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+signal start_game
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	$MessageLabel.text = "Stay Safe Bot"
+	$MessageLabel.show()
+	yield(get_tree().create_timer(1), 'timeout')
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+
+func _on_HUD_start_game():
+	pass # Replace with function body.
